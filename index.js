e4e2a93175344df13d20e9a579521a5d09219801
alazzuri <alexislazzuri@gmail.com>
2021-05-02 20:22:29 -0300

    Se agregaron funciones para realizar cálculos aritméticos

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function showNumber(number) {
   console.log("El número ingresado es" + " " + number);
 }
+
+function sumar(numero1, numero2) {
+  console.log(numero1 + numero2);
+}
+
+function restar(numero1, numero2) {
+  console.log(numero1 - numero2);
+}
+
+function multiplicar(numero1, numero2) {
+  console.log(numero1 * numero2);
+}
+
+function dividir(numero1, numero2) {
+  console.log(numero1 / numero2);
+}
